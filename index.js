d3cb3a8828e59a3d8d51ae13449d68bde023d76c
Neil Prabhu <neilprabhu96@gmail.com>
2018-09-13 14:48:52 -0700

    added users

--- index.js
+++ index.js
@@ -9,16 +9,11 @@ var http = require('http');
 var https = require('https');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
-var config = require('./config');
+var config = require('./lib/config');
 var fs = require('fs');
-var _data = require('./lib/data');
+var handlers = require('./lib/handlers');
+var helpers = require('./lib/helpers');
 
-
-// TESTING
-// @TODO delete this
-_data.delete('test','newFile',function(err,data){
-  console.log('this was the error ',err);
-});
 
 // Instantiate the http server
 var httpServer = http.createServer(function(req,res) {
@@ -82,7 +77,7 @@ var unifiedServer = function(req,res){
       'queryStringObject' : queryStringObject,
       'method' : method,
       'headers' : headers,
-      'payload' : buffer
+      'payload' : helpers.parseJsonToObject(buffer)
     };
 
     // Route the request to the handler specified in the router
@@ -106,20 +101,9 @@ var unifiedServer = function(req,res){
   });
 };
 
-// Definer the handlers
-var handlers = {};
-
-// Ping handler
-handlers.ping = function(data,callback){
-  callback(200);
-};
-
-// Not found handler
-handlers.notFound = function(data,callback){
-  callback(404);
-};
 
 // Define a request router
 var router = {
-  'ping' : handlers.ping
+  'ping' : handlers.ping,
+  'users': handlers.users
 };
